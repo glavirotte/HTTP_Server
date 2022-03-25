03b16f9a6990e792ad1081e8d77fba91e1e68781
Emeric Breton <emeric.breton@etu.univ-grenoble-alpes.fr>
2022-03-25 10:17:55 +0100

    Step1 - Serving static pages done

--- ricm.web.GIVEN/impl/httpserver/itf/impl/HttpStaticRequest.java
+++ ricm.web.GIVEN/impl/httpserver/itf/impl/HttpStaticRequest.java
@@ -1,7 +1,9 @@
 package httpserver.itf.impl;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-
+import java.io.PrintStream;
 
 import httpserver.itf.HttpRequest;
 import httpserver.itf.HttpResponse;
@@ -17,7 +19,26 @@ public class HttpStaticRequest extends HttpRequest {
 	}
 	
 	public void process(HttpResponse resp) throws Exception {
-	// TO COMPLETE
+		if (m_ressname.equals("/FILES/")) {
+			m_ressname = "/FILES/"+DEFAULT_FILE;
+		}
+		
+		if (m_method.equals("GET")) {
+			File ressource = new File(m_hs.getFolder()+m_ressname);
+			
+			if (ressource.exists()) {
+				FileInputStream fis = new FileInputStream(ressource);
+				resp.setReplyOk();
+				resp.setContentLength((int) ressource.length());
+				resp.setContentType(HttpRequest.getContentType(m_ressname));
+				PrintStream ps = resp.beginBody();
+				ps.write(fis.readAllBytes());
+				fis.close();
+			}
+			else {
+				resp.setReplyError(404, "File not found");
+			}
+		}
 	}
 
 }
